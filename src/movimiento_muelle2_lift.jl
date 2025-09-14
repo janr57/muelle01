790955edd48bc35150ab26840e2450fc084000d8
janr <janr.devel@gmail.com>
2025-09-14 12:09:48 +0200

    Eliminación de comentarios superfluos

--- src/movimiento_muelle2_lift.jl
+++ src/movimiento_muelle2_lift.jl
@@ -60,65 +60,6 @@ for i in 2:len
     sleep(0.001)
 end
 
-
-
-
-
-
-
-
-#pos2 = @lift(Point2f(x2_tuple[$index], y))
-
-# Step 2: Use @lift to define the point's position
-#point_pos = @lift(Point2f(cos(deg2rad($angle)), sin(deg2rad($angle))))
-
-# Step 3: Plot the point
-#scatter!(ax, point_pos, markersize = 20, color = :red)
-
-
-
-
-
-
-# 2)  Definición de las posiciones x1 y x2 como observables
-#x1_ini = x1_tuple[1]
-#x2_ini = x2_tuple[1]
-
-
-
-#pos1 = Observable(Point2f(x1_ini, y))
-#pos2 = Observable(Point2f(x2_ini, y))
-#muelle = Observable((pos1, pos2))
-
-
-## Step 2: Use @lift to define the point's position
-#muelle = @lift(Point2f(cos(deg2rad($angle)), sin(deg2rad($angle))))
-
-## 3) Representación de las masas
-##lines!(ax, [x1_ini, x2_ini], [y, y], color = :red)
-#lines!(ax, [muelle[][1][][1], muelle[][2][][1]], [0.0, 0.0], color = :black)
-#scatter!(ax, pos1, markersize = 20, color = :black)
-#scatter!(ax, pos2, markersize = 20, color = :black)
-#
-#display(f)
-#
-## 4) Animación del sistema
-#for i in 2:len
-#    # Nuevas coordenadas
-#    x1_new = x1_tuple[i]
-#    x2_new = x2_tuple[i]
-#    
-#    # Nuevos valores de los observables
-#    pos1[] = Point2f(x1_new, y)
-#    pos2[] = Point2f(x2_new, y)
-#    muelle[] = (pos1, pos2)
-#    #notify!(muelle)
-#
-#    #lines!(ax, [x1_new, x2_new], [y, y], linewidth = 1, color = :blue)
-#
-#    sleep(0.005)
-#end
-
 println("FIN")
 
 
